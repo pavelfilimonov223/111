4c3905b8bc18857dc0526f879e192694c972db0c
Pavel_Filimonov <filimonov223@yandex.ru>
2023-10-07 22:19:27 +0500

    Сообщение 005 - есть ошибка

---
+++ Example005_if_else/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 005 про Если и Иначе
+Console.Write("Введите имя пользователя");
+string username=Console.ReadLine();
+if(username.ToLower()=="Павел")
+{
+    Console.WriteLine(Павел, ты молодец);
+}
+else
+{
+    Console.Write("Добрый день, ");
+    Console.WriteLine(username);
+}
+
+
