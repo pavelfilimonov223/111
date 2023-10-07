1215160dd4ec25e9ce63a1e6da64e4247673e882
Pavel_Filimonov <filimonov223@yandex.ru>
2023-10-07 23:53:00 +0500

    Сообщение 006 про гири

---
+++ Example006_Base/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 006 с гирями, находим самую тяжелую.
+int a = 1;
+int b = 3;
+int c = 8;
+int d = 14;
+int e = 5;
+int max = a;
+if(a>max) max=a;
+if(b>max) max=b;
+if(c>max) max=c;
+if(d>max) max=d;
+if(e>max) max=e;
+Console.Write("max=");
+Console.WriteLine(max);
