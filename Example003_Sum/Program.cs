168c4a315ef92da1fa5abc0713b6f6bbbbc32727
Pavel_Filimonov <filimonov223@yandex.ru>
2023-10-07 21:02:36 +0500

    Сообщение 003 про Random

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,6 +1,13 @@
 ﻿// Задача 003 на сложение чисел
-int numberA = 4;
-int nunberB = 15;
+// int numberA = 4;
+// int nunberB = 15;
+// int result = numberA + nunberB;
+// Console.WriteLine(result);
+
+int numberA = new Random().Next(1,10);
+Console.WriteLine(numberA);
+int nunberB = new Random().Next(1,10);
+Console.WriteLine(nunberB);
 int result = numberA + nunberB;
 Console.WriteLine(result);
 
