d36ab689858611ca58387a904013c9a2333d5f6a
Pavel_Filimonov <filimonov223@yandex.ru>
2023-10-08 06:31:54 +0500

    Сообщение 005 исправил ошибку в строке 4 надо имя с маленькой буквы

--- Example005_if_else/Program.cs
+++ Example005_if_else/Program.cs
@@ -1,9 +1,9 @@
 ﻿// Задача 005 про Если и Иначе
-Console.Write("Введите имя пользователя");
+Console.Write("Введите имя пользователя ");
 string username=Console.ReadLine();
-if(username.ToLower()=="Павел")
+if(username.ToLower()=="павел")
 {
-    Console.WriteLine(Павел, ты молодец);
+    Console.WriteLine("Павел, ты молодец !!!");
 }
 else
 {
