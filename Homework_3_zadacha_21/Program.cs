d945ed5f16fc15e03a7dc166d2c069deea989a78
Pavel_Filimonov <filimonov223@yandex.ru>
2023-10-17 17:24:58 +0500

    Сделал ДЗ к семинару 3, задачи № 19, 21, 23

---
+++ Homework_3_zadacha_21/Program.cs
@@ -0,0 +1,23 @@
+﻿// По координатам двух точек в 3D пространстве найти расстояние между ними 
+// (по теореме Пифагора).
+double FindDist(double xa, double ya, double xb, double yb, double za, double zb)
+{
+    return Math.Round(Math.Sqrt(Math.Pow(xb-xa, 2) + Math.Pow(yb-ya,2) + Math.Pow(zb-za,2)), 2);
+}
+System.Console.Write("Введите XA: ");
+double xa = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите YA: ");
+double ya = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите ZA: ");
+double za = Convert.ToInt32(Console.ReadLine());
+
+System.Console.Write("Введите XB: ");
+double xb = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите YB: ");
+double yb = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите ZB: ");
+double zb = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine($"A({xa},{ya},{za}), B({xb},{yb},{zb}) -> {FindDist(xa, ya, xb, yb, za, zb)}");
+
+
---
+++ Homework_3_zadacha_23/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 23. Принимает число N. Выдает таблицу кубов чисел от 1 до N.
+
+void TablCub(int n)
+{
+    int cur = 1;
+    while (cur <= n)
+    {
+        System.Console.WriteLine($"{cur * cur * cur}");
+        cur++;
+    }
+} 
+System.Console.Write("Введи номер N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+TablCub(n);
+
+
